c4c0f77fe3927734fe9864d7ec221340e716ef7c
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-11-05 19:59:30 +0000

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,57 @@
  for you to use if you need it!
  */
 
+function createEmployeeRecord(array){
+    let newObj = {}
+      newObj.firstName = array[0];
+      newObj.familyName = array[1];
+      newObj.title = array[2];
+      newObj.payPerHour = array[3];
+      newObj.timeInEvents = [];
+      newObj.timeOutEvents = [];
+      return newObj
+  }
+
+  function createEmployeeRecords(array){
+    let newArrayOfObj = []
+    array.forEach(element => {
+        newArrayOfObj.push(createEmployeeRecord(element))
+    });
+    return newArrayOfObj
+}
+
+let createTimeInEvent = function (dateStamp){
+
+    let hour = parseInt(dateStamp.split(" ")[1]);
+    let date = dateStamp.split(" ")[0];
+    this.timeInEvents.push({type: "TimeIn", hour: hour, date: date })
+    return this
+}
+
+let createTimeOutEvent = function (dateStamp){
+    let hour = parseInt(dateStamp.split(" ")[1]);
+    let date = dateStamp.split(" ")[0];
+    this.timeOutEvents.push({type: "TimeOut", hour: hour, date: date })
+    return this
+}
+
+let hoursWorkedOnDate = function (date){
+    // console.log(this)
+   let timeIn = this.timeInEvents.find(function(element){
+   return element.date == date
+    }).hour
+    let timeOut = this.timeOutEvents.find(function(element){
+    return element.date == date
+    }).hour
+   return (timeOut - timeIn)/100
+}
+
+let wagesEarnedOnDate = function(date){
+    let payPerHour = this.payPerHour
+    let hoursWorked = hoursWorkedOnDate.call(this, date)
+    return payPerHour * hoursWorked
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -20,3 +71,22 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function calculatePayroll(array){
+    let wages = []
+    array.forEach(function(employee){
+        wages.push(allWagesFor.call(employee))
+    })
+    let total = wages.reduce(function(sum, num) { 
+        return sum + num; 
+    })
+    return total
+}
+
+function findEmployeeByFirstName(array, firstName){
+    
+    let object = array.find(function(employee){
+     return employee.firstName = firstName
+     })
+     return object
+    }
